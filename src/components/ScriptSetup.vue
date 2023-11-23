89bc22d39924578d957cb01b9bca417c63517a40
lsuhan00 <lsuhan00@hanmail.net>
2023-11-23 14:10:54 +0900

    Vue3 완벽 마스터: 기초부터 실전까지 - "기본편" 완강

---
+++ src/components/ScriptSetup.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="container py-4">
+		{{ msg }}
+		<input v-model="message" type="text " />
+		<br />
+		{{ message }}
+	</div>
+	<button @click="sayHello">클릭</button>
+	<PostItem type="news" title="제목" contents="내용" :is-like="true"></PostItem>
+
+	<PostItem type="news" title="제목" contents="내용" :is-like="true"></PostItem>
+
+	<PostItem type="news" title="제목" contents="내용" :is-like="true"></PostItem>
+
+	<PostItem type="news" title="제목" contents="내용" :is-like="true"></PostItem>
+
+	<TemplateRefsChild ref="child"></TemplateRefsChild>
+	<template v-if="child">
+		{{ child.message }}
+	</template>
+	<hr />
+	<MyButton class="parent-class"></MyButton>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import PostItem from '@/components/setup/PostItem.vue';
+import TemplateRefsChild from './TemplateRefsChild.vue';
+import MyButton from '@/components/setup/MyButton.vue';
+
+const msg = 'Hello world';
+const message = ref('');
+const sayHello = () => {
+	alert('hello wold');
+};
+
+const child = ref(null);s
+defineExpose({
+	msg,
+});
+
+const response = await axios('https://dummy.restapiexample.com/api/v1/employees');
+console.log(response)
+</script>
+
+<style lang="scss" scoped></style>
